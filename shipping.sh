fd2e65b8ea91fdf19f9a5c9c6ee091959dec7b2b
“jagadish7-25” <,kadapajag9@gmail.com>
2023-08-25 11:52:41 +0200

    roboshop codes updated

--- shipping.sh
+++ shipping.sh
@@ -9,7 +9,7 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
 yum install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql.me25.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 systemctl daemon-reload
 systemctl enable shipping
